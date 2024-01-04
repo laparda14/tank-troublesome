cafd39aee16dbf4314b587bcfbbad0f21d060874
laparda14 <120476892+laparda14@users.noreply.github.com>
2024-01-04 17:02:59 -0500

    Update player.js

--- player.js
+++ player.js
@@ -66,7 +66,7 @@ Player.prototype.move = function (forward){
 	}
 };
 
-Player.prototype.create_bullet = function (speed=20, r=6, life=900000000){
+Player.prototype.create_bullet = function (speed=2, r=20, life=900000000){
 	const dist = this.length/2 + r;
 	const pos = {x:this.pos.x + cos(this.angle)*dist, y:this.pos.y + sin(this.angle)*dist};
 
